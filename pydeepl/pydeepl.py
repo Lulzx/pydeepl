67d7b19416d5268ee1034ee38e54c15bbc469d47
EmilioK <emiliovirtual@hotmail.com>
2017-09-01 20:24:56 +0200

    First commit

---
+++ pydeepl/pydeepl.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+BASE_URL = 'https://www.deepl.com/jsonrpc'
+
+LANGUAGES = {
+    'DE': 'German',
+    'EN': 'English',
+    'FR': 'French',
+    'ES': 'Spanish',
+    'IT': 'Italian',
+    'NL': 'Dutch',
+    'PL': 'Polish'
+}
+
+
+class TranslationError(Exception):
+    def __init__(self, message, errors):
+        super(TranslationError, self).__init__(message)
+        self.errors = errors
+
+
+def translate(text, from_lang, to_lang):
+    if text is None:
+        raise TranslationError('Text can\'t be None.', {})
+    if to_lang not in LANGUAGES.keys():
+        raise TranslationError('Language {} not available.'.format(to_lang), {})
+    if from_lang is not None and from_lang not in LANGUAGES.keys():
+        raise TranslationError('Language {} not available.'.format(from_lang), {})
+
+    parameters = {
+        'jsonrpc': '2.0',
+        'method': 'LMT_handle_jobs',
+        'params': {
+            'jobs': [
+                {
+                    'kind':'default',
+                    'raw_en_sentence': text
+                }
+            ],
+            'lang': {
+                'user_preferred_langs': [
+                    from_lang,
+                    to_lang
+                ],
+                'source_lang_user_selected': from_lang,
+                'target_lang': to_lang
+            },
+            'priority': -1
+        },
+    }
+
+    response = json.loads(requests.post(BASE_URL, json=parameters).text)
+
+    print(response)
+
+    translations = response['result']['translations']
+
+    if len(translations) == 0 or translations[0]['beams'] is None or translations[0]['beams'][0]['postprocessed_sentence'] is None:
+        raise TranslationError('No translations found.', {})
+
+    return translations[0]['beams'][0]['postprocessed_sentence']
---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from distutils.core import setup
+setup(
+  name='pydeepl',
+  packages=['pydeepl'],
+  version='0.1',
+  description='An API wrapper for deepl.com translating service.',
+  author='Emilio Carrión Peñalba',
+  author_email='emiliok1997@gmail.com',
+  url='https://github.com/EmilioK97/pydeepl',
+  download_url='https://github.com/emiliok97/pydeepl/archive/0.1.tar.gz',
+  keywords=['translate', 'translation', 'example'],
+  classifiers=[],
+  install_requires=[
+    'requests>=2.18.4',
+  ],
+)
